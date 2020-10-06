df03416ff5396651f6d4f88655a4bfde4f3d42c0
Jennifer Henry <jenhenry1995@gmail.com>
2020-10-06 16:09:32 -0700

    created modals on portfolio page

---
+++ portfolioScript.js
@@ -0,0 +1,4 @@
+$('#yelpLink').on('click', () => window.open('https://jenryhennifer.github.io/dreamteamrepo/'))
+
+$('#scheduleLink').on('click', () => window.open('https://jenryhennifer.github.io/workDaySchedule/'))
+
