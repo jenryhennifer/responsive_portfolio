24a3ab2c0f0683aac806b6c30db83449389ac2db
Jennifer Henry <jenhenry1995@gmail.com>
2020-10-06 17:00:05 -0700

    added new font family and medium link

--- portfolioScript.js
+++ portfolioScript.js
@@ -1,4 +1,7 @@
+//click links for the portfolio page to open to the deployment links for each app
+
 $('#yelpLink').on('click', () => window.open('https://jenryhennifer.github.io/dreamteamrepo/'))
 
 $('#scheduleLink').on('click', () => window.open('https://jenryhennifer.github.io/workDaySchedule/'))
 
+$('#passwordLink').on('click', () => window.open('https://jenryhennifer.github.io/passwordGenerator/'))
