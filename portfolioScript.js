0c159021651bb270733ea51a9f676739815ce3f5
Jennifer Henry <jenhenry1995@gmail.com>
2020-10-07 19:09:20 -0700

    changed style/design/background

--- portfolioScript.js
+++ portfolioScript.js
@@ -1,7 +1,23 @@
 //click links for the portfolio page to open to the deployment links for each app
 
-$('#yelpLink').on('click', () => window.open('https://jenryhennifer.github.io/dreamteamrepo/'))
+$('#yelpDeploy').on('click', () => window.open('https://jenryhennifer.github.io/dreamteamrepo/'))
+$('#yelpRepo').on('click')
 
-$('#scheduleLink').on('click', () => window.open('https://jenryhennifer.github.io/workDaySchedule/'))
+$('#scheduleDeploy').on('click', () => window.open('https://jenryhennifer.github.io/workDaySchedule/'))
+$('#scheduleRepo').on('click')
 
 $('#passwordLink').on('click', () => window.open('https://jenryhennifer.github.io/passwordGenerator/'))
+
+
+//Email Submit Attempt
+
+$('#emailSubmit').on('click', function(event){
+    event.preventDefault();
+    let name = $('#userName').val();
+    let subjectLine = $('#subjectLine').val();
+    let message = $('#message').val();
+    let subject = name + ': ' + subjectLine
+
+
+    document.location = 'mailto:jenhenry1995@gmail.com?subject='+subject+'&body='+message + name;
+});
